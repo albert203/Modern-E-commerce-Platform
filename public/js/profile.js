7d2e7d6c8c98714db429d3d7609f1a5b737722f0
albert <albert.jordaan@outlook.com>
2024-05-27 19:29:27 +1200

    added edit profile settings button, not full functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,45 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    profileButton = document.querySelector('.profile-edit-button');
-    profileButton.document.addEventListener('click', async () => {
+const editButton = document.querySelector('.profile-edit-button');
+const cancelButton = document.querySelector('.profile-cancel-button');
+const applyButton = document.querySelector('.profile-apply-button');
 
-    });
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    if (editButton){
+        editButton.addEventListener('click', async (e) => {
+            e.preventDefault();
+            editButton.classList.add('hidden');
+            cancelButton.parentElement.classList.remove('hidden');
+            applyButton.parentElement.classList.remove('hidden');
+            
+        });
+    } else if (cancelButton){
+        cancelButton.addEventListener('click', async (e) => {
+            e.preventDefault();
+            cancelButton.parentElement.classList.remove('hidden');
+            cancelButton.parentElement.classList.remove('hidden');
+        });
+
+    } else if (applyButton){
+        applyButton.addEventListener('click', async (e) => {
+            e.preventDefault();
+            applyButton.classList.add('hidden');
+            editButton.classList.remove('hidden');
+        });
+
+    }
+
+    
+    
 });
+
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+    formOpenBtn.textContent = "Signup"
+  });
+  loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+    formOpenBtn.textContent = "Login"
+  });
